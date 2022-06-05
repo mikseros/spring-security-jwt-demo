0e59d05b468b94e85ce4b2f879a49d83dbed51c4
mikseros <mmadynski@gmail.com>
2022-06-05 14:16:19 +0200

    Entities (Models) created

--- src/main/java/com/mikseros/springsecjwtdemo/HelloResource.java
+++ src/main/java/com/mikseros/springsecjwtdemo/HelloResource.java
@@ -1,12 +1,12 @@
 package com.mikseros.springsecjwtdemo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
 
-	@RequestMapping({"/hello"})
+	@RequestMapping( "/hello" )
 	public String hello() {
 		return "Hello in Security-Demo!";
 	}
--- src/main/java/com/mikseros/springsecjwtdemo/SecurityConfigurer.java
+++ src/main/java/com/mikseros/springsecjwtdemo/SecurityConfigurer.java
@@ -1,10 +1,13 @@
 package com.mikseros.springsecjwtdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.mikseros.springsecjwtdemo.services.MyUserDetailsService;
 
@@ -17,6 +20,11 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 	
