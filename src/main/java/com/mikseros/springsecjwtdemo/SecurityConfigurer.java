3ed4bb8ff96dcd53c7426082fda6b9c9d3dea32b
mikseros <mmadynski@gmail.com>
2022-06-05 13:40:52 +0200

    UserDetailsService+SecurityConfigurer

---
+++ src/main/java/com/mikseros/springsecjwtdemo/SecurityConfigurer.java
@@ -0,0 +1,24 @@
+package com.mikseros.springsecjwtdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.mikseros.springsecjwtdemo.services.MyUserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailsService);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/mikseros/springsecjwtdemo/services/MyUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.mikseros.springsecjwtdemo.services;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		return new User("John", "password", new ArrayList<>());
+	}
+
+}
